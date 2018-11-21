b391ad44881014562fa8369b228adcbb97dc109f
Ethan Su <ethan.su.15@gmail.com>
2018-11-21 12:58:52 -0800

    working cue component

--- src/components/Home/cuevideos.js
+++ src/components/Home/cuevideos.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
-import VideoPlayer from "react-simple-video-player";
 import "../../css/cuevideos.css";
-import squatdepth1 from "../../videos/squatdeepdata.mp4";
+
 export default class CueVideos extends Component {
   render() {
     return (
       <div>
-        <section className="cuesection">
-          <div className="indivcue">
-            <div id="cuename">Squat Depth</div>
+        <section className="cuesection1">
+          <div id="cuename">{this.props.cueName}</div>
+          <div className="indivcue1">
             <video
               id="cuevideo1"
-              src={squatdepth1}
+              src={require(`../../videos/${this.props.videoName1}`)}
               type="video\mp4"
               autoPlay
               loop
               muted
             />
-            <div id="aboutcue1">Something about Squat Depth</div>
+            <div id="aboutcue1">{this.props.aboutCue1}</div>
+          </div>
+        </section>
+        <section className="cuesection2">
+          <div className="indivcue2">
+            <video
+              id="cuevideo2"
+              src={require(`../../videos/${this.props.videoName2}`)}
+              type="video\mp4"
+              autoPlay
+              loop
+              muted
+            />
+            <div id="aboutcue2">{this.props.aboutCue2}</div>
           </div>
         </section>
       </div>
--- src/components/MainPages/home.js
+++ src/components/MainPages/home.js
@@ -10,7 +10,20 @@ export default class Home extends Component {
       <div>
         <Title />
         <div className="cueclass">Cues</div>
-        <CueVideos />
+        <CueVideos
+          cueName={"Squat Depth"}
+          videoName1={"squatdeepdata.mp4"}
+          videoName2={"squatnotdeepdata.mp4"}
+          aboutCue1={"Something about Squat Depth 1."}
+          aboutCue2={"Something about Squat Depth 2."}
+        />
+        <CueVideos
+          cueName={"Shoulder Alignment"}
+          videoName1={"shoulderaligndata.mp4"}
+          videoName2={"shouldernotalignleftdata.mp4"}
+          aboutCue1={"Something about Shoulder Alignment 1."}
+          aboutCue2={"Something about Shoulder Alignment 2."}
+        />
       </div>
     );
   }
